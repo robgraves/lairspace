7ac05fa57d36da5e020753cddbf23eff076e5625
dsoutha3 <dsoutha3@corning-cc.edu>
2015-03-25 22:09:33 -0400

    my be on to something with this viewport funtion I ave finally found to get sectors

--- inc/general.h
+++ inc/general.h
@@ -12,9 +12,6 @@
 
 void createWindow();
 
-const int SCREEN_WIDTH = 640;
-const int SCREEN_HEIGHT = 480;
-
 using namespace std;
 
 
--- src/lairspace.cpp
+++ src/lairspace.cpp
@@ -1,40 +1,56 @@
 #include "general.h"
 
+const int SCREEN_WIDTH = 1300;
+const int SCREEN_HEIGHT = 800;
+
 SDL_Rect shipPos;
-SDL_Renderer * renderer=NULL; //To make global use
+SDL_Renderer *renderer=NULL; //To make global use
 SDL_Window *window=NULL; //always set any pointer to NULL prevent mem leaks
 SDL_Surface *screen=NULL;
+
+//Quad functionality
+SDL_Rect quad1;
+SDL_Rect quad2;
+SDL_Rect quad3;
+SDL_Rect quad4;
 
 int main(int argc, char ** argv)
 {
     
-    bool quit = false;
-    SDL_Event event;
+	bool quit = false;
+	SDL_Event event;
 
-    SDL_Init(SDL_INIT_VIDEO);
-    IMG_Init(IMG_INIT_PNG);
-
-    createWindow();
-    SDL_Surface * image = IMG_Load ("../img/playerShip.png"); 
-    SDL_Texture * texture = SDL_CreateTextureFromSurface(renderer, image);
-	int shipW=image->w;
+	SDL_Init(SDL_INIT_VIDEO);
+	IMG_Init(IMG_INIT_PNG);
+	createWindow();
+    
+	SDL_Surface * image = IMG_Load ("../img/playerShip.png"); 
+	SDL_Texture * texture = SDL_CreateTextureFromSurface(renderer, image);
+	int shipW=image->w; //getting size of the ship.
 	int shipH=image->h;    
-	shipPos= { ((SCREEN_HEIGHT/2)+shipW),((SCREEN_HEIGHT/2)-shipH),shipW,shipH };	 
-    //game loop
-    while (!quit)
-    {
-        SDL_WaitEvent(&event);
-        switch(event.type)
-        {
-        case SDL_QUIT:
-            quit = true;
-            break;
-        }   
+	shipPos= { ((SCREEN_WIDTH/2)-shipW),((SCREEN_HEIGHT/2)-shipH),shipW,shipH };	 
+   
+	quad2.x = SCREEN_WIDTH / 2; 
+	quad2.y = 0; 
+	quad2.w = SCREEN_WIDTH / 2; 
+	quad2.h = SCREEN_HEIGHT / 2;
+	SDL_RenderSetViewport(renderer,&quad2); 	
+
+	//game loop
+	while (!quit)
+	{
+        	SDL_WaitEvent(&event);
+        	switch(event.type)
+        	{
+        		case SDL_QUIT:
+            		quit = true;
+            		break;
+        	}   
         SDL_RenderClear( renderer );
         SDL_RenderCopy(renderer, texture, NULL, &shipPos);
         SDL_RenderPresent( renderer );
-
-    }
+	
+	}
 
     //cleanup
     SDL_DestroyTexture(texture);
